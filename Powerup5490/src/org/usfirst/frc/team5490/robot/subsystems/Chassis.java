2cbe14ad3999d0e24cfe68e6f628bcf1d3877650
derbyw <derbyw@verizon.net>
2018-01-21 18:06:54 -0500

    Added support for IMU board

--- Powerup5490/src/org/usfirst/frc/team5490/robot/subsystems/Chassis.java
+++ Powerup5490/src/org/usfirst/frc/team5490/robot/subsystems/Chassis.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
 import org.usfirst.frc.team5490.robot.RobotMap;
+import org.usfirst.frc.team5490.robot.commands.DriveRobot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
@@ -15,6 +16,8 @@ import edu.wpi.first.wpilibj.Joystick;
 
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.analog.adis16448.frc.ADIS16448_IMU;
 
 /**
  *
@@ -29,6 +32,8 @@ public class Chassis extends Subsystem {
     SpeedController motorRearRight = new Talon(RobotMap.mtrRearRight);
     
     MecanumDrive m_robotDrive = new MecanumDrive(motorFrontLeft,motorRearLeft,motorFrontRight,motorRearRight);
+    
+    ADIS16448_IMU imu = new ADIS16448_IMU();
     
     // Winch objects
     SpeedController motorWinch = new Talon(RobotMap.mtrWinch);
@@ -62,8 +67,30 @@ public class Chassis extends Subsystem {
 		// ToDo calculate based on Drum size
 		m_WinchEncoder.setDistancePerPulse((4.0 / 12.0 * Math.PI) / 360.0);
 		
+
+		// ToDo determine when the light should come on/off
 		addChild("Lightmast", m_lightmast);
-		//
+
+		
+		//When no other command is running let the operator drive around using the joystick		 
+		setDefaultCommand(new DriveRobot());
+		
+		
+		SmartDashboard.putNumber("Gyro-X", imu.getAngleX());
+		SmartDashboard.putNumber("Gyro-Y", imu.getAngleY());
+		SmartDashboard.putNumber("Gyro-Z", imu.getAngleZ());
+		
+		SmartDashboard.putNumber("Accel-X", imu.getAccelX());
+		SmartDashboard.putNumber("Accel-Y", imu.getAccelY());
+		SmartDashboard.putNumber("Accel-Z", imu.getAccelZ());
+		
+		SmartDashboard.putNumber("Pitch", imu.getPitch());
+		SmartDashboard.putNumber("Roll", imu.getRoll());
+		SmartDashboard.putNumber("Yaw", imu.getYaw());
+		
+		SmartDashboard.putNumber("Pressure: ", imu.getBarometricPressure());
+		SmartDashboard.putNumber("Temperature: ", imu.getTemperature());
+		
 		
     }
     
@@ -85,6 +112,11 @@ public class Chassis extends Subsystem {
 	{
 		double speed = (-1*driveStick.getThrottle()+1)/2;
 		m_robotDrive.driveCartesian(speed*driveStick.getY(), -speed*driveStick.getX(), speed*driveStick.getTwist(), 0);
+	}
+	
+	public void StopMotors()
+	{		
+		m_robotDrive.driveCartesian(0,0,0,0);		
 	}
 	
 	/**
@@ -121,7 +153,5 @@ public class Chassis extends Subsystem {
 	public boolean isLiftStored() {
 		return m_lstored.get();
 	}
-
-	
 }
 
