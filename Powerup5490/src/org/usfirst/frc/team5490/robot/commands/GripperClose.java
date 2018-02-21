da2c16eaf281bb1f406fe738b1a6765408565369
Chae Park <light2857@gmail.com>
2018-02-21 15:13:03 -0500

    Additions & edits to commands, mainly for testing motors

--- Powerup5490/src/org/usfirst/frc/team5490/robot/commands/GripperClose.java
+++ Powerup5490/src/org/usfirst/frc/team5490/robot/commands/GripperClose.java
@@ -5,30 +5,37 @@ import org.usfirst.frc.team5490.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Command for manually closing the gripper.
  */
 public class GripperClose extends Command {
 
-    public GripperClose() {
-    	
+	public double rateL;
+	public double rateR;
+
+	public GripperClose() {
     	requires(Robot.m_Gripper);
+    	this.rateL = 0.05;
+    	this.rateR = 0.05;
+    }
+	
+    public GripperClose(double rateL, double rateR) {
+    	requires(Robot.m_Gripper);
+    	this.rateL = rateL;
+    	this.rateR = rateR;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	Robot.m_Gripper.close(rateL, rateR);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.m_Gripper.close(1);
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	
-    	return Robot.m_Gripper.isGrabbing();        
+    	return false;
     }
 
     // Called once after isFinished returns true
